21de08485cbf7c575b8c28de3b43ff0297cc61de
Mia5391 <mariakrasteva5391@gmail.com>
2018-06-28 00:46:08 -0400

    added calculator html, calculated balance not working

--- calculator.js
+++ calculator.js
@@ -1,16 +1,32 @@
 window.onload = function() {
 
-function getBipByNumber (num) {
-  fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${num}`)
-    .then(response => response.json())
-    .then(data => {
-      //console.log(data);
-      Object.keys(data).forEach(function(key) {
-        if(key === 'Saldo  tarjeta'){
-          document.getElementById('balance').innerHTML = data[key];
-        }
-      });
-   })
+  document.getElementById("calculate").onclick = function(){
+    let card = document.getElementById("card-select").value;
+    let tariff = document.getElementById("tariff-select").value;
+    let balance = getBalanceNumber(getBipBalanceByNumber(card));
+    document.getElementById("balance").innerHTML = calculateJourney(balance, tariff);
   }
 
+  function getBipBalanceByNumber (num) {
+    let cardBalance = '';
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${num}`)
+      .then(response => response.json())
+      .then(data => {
+        //console.log(data);
+        Object.keys(data).forEach(function(key) {
+          if(key === 'Saldo  tarjeta'){
+            cardBalance = data[key];
+          }
+        });
+    });
+    return cardBalance;
+  }
+
+  function getBalanceNumber(balanceText) {
+    return balanceText.replace('$','').replace('.','');
+  }
+
+  function calculateJourney(cardBalance, tariff){
+    return cardBalance-tariff;
+  }
 }
