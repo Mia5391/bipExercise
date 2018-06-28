b6809476be662f7caab4c7f6a04e30ac152ebcde
Mia5391 <mariakrasteva5391@gmail.com>
2018-06-27 23:53:53 -0400

    API call working with selector

--- balance.js
+++ balance.js
@@ -1,12 +1,19 @@
 window.onload = function() {
-  
+
+document.getElementById("balance-button").onclick = function(){
+  getBipByNumber(document.getElementById("card-select").value);
+}
+
 function getBipByNumber (num) {
   fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${num}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      //console.log(data);
+      Object.keys(data).forEach(function(key) {
+        if(key === 'Saldo  tarjeta'){
+          document.getElementById('balance').innerHTML = data[key];
+        }
+      });
    })
   }
-console.log(getBipByNumber('26947792'));
-
 }
