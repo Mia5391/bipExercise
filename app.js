a69a54af55921598f319a07a8bf8c861359f7db5
Mia5391 <mariakrasteva5391@gmail.com>
2018-06-27 21:45:49 -0400

    login without validations, second page linked

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const userEmail = "mia@gmail.com"
-const userPassword = "12345678"
-
-function userLogin(email, password){
-   if (userEmail && userPassword = true) {
-location="page2.html"
-   }else {
-     alert("Invalid username or password")
-}
-}
+window.onload = function() {
+  const userEmail = "mia@gmail.com";
+  const userPassword = "12345678";
 
 
-function getBipByNumber (num) {
-  fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${num}`)
-  .then(response => response.json())
-  .then(data => {
-
-      console.log(data);
-
-   })
+  document.getElementById("login").onclick = function (email, password){
+    let userEmailInput = document.getElementById("user-email").value;
+    let userPasswordInput = document.getElementById("user-password").value;
+    if (userEmail === userEmailInput && userPassword === userPasswordInput) {
+      location="page2.html"} else {
+      alert("Invalid username or password");
+    }
   }
 
-})
+  function getBipByNumber (num) {
+    fetch(`http://www.psep.cl/api/Bip.php?&numberBip=${num}`).then(response => response.json()).then(data => {
+
+        console.log(data);
+
+     })
+    }
+  console.log(getBipByNumber('26947792'));
+
 }
-console.log(getBipByNumber('26947792'))
