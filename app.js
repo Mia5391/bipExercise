297b5ae24d958a43cd11338b971b11a5a7e1795d
Mia5391 <mariakrasteva5391@gmail.com>
2018-07-03 21:51:11 -0400

    added local storage of username, printing welcome message

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 window.onload = function() {
   const defaultUserEmail = "mia@gmail.com";
-  const defaultUserPassword = "12345678";
-
+  const defaultUserPassword = "123";
 
   document.getElementById("login").onclick = function (email, password){
     let userEmailInput = document.getElementById("user-email").value;
     let userPasswordInput = document.getElementById("user-password").value;
     if (validateEmailAndPassword(userEmailInput, userPasswordInput)) {
-      location="page2.html"
+      let userName = document.getElementById('user-name').value;
+      localStorage.setItem('user-name', userName);
+      document.getElementById('user-name').innerHTML = 'Mia';
+      location="page2.html";
     } else {
       alert("Invalid username or password");
     }
--- options.js
+++ options.js
@@ -1,9 +1,17 @@
 window.onload = function(){
+
+  printUserName();
+
   document.getElementById("balance").onclick = function (){
     location="balance.html"
   }
 
   document.getElementById("calculator").onclick = function (){
     location="calculator.html"
+  }
+
+  function printUserName(){
+    let welcomeMessage = localStorage.getItem('user-name');
+    document.getElementById('welcomeMessage').innerHTML = "Welcome " + welcomeMessage;
   }
 }
